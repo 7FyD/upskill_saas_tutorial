f646c534c9d326aaad62ca87197beae77b05b210
Alex Vlad <alexvlad1313@gmail.com>
2022-05-25 21:03:45 +0000

    Added comments and made inclusion of @ or . in email form field mandatory

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,40 @@
 class ContactsController < ApplicationController
+ 
+  # GET request to /contact-us
+  # show new contact form
   def new
     @contact = Contact.new
   end
   
+  # POST request /contacts
   def create
+    # mass assigment of form fields into contact object
     @contact = Contact.new(contact_params)
+    # save the Contact object to the database
     if @contact.save
+      # store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:message]
+      # plug variables into contact mailer email method 
+      # and send the mail
       ContactMailer.contact_email(name, email, body).deliver
+      # store message in flash hash
+      # and redirect to the new action
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
+      # if saving fails, store error message
+      # and redirect to the new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
   
   private
+  # collect data from form using secure
+  # Rails, "strong params" and whitelist
+  # the form fields.
     def contact_params
       params.require(:contact).permit(:name, :email, :message)
     end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ActiveRecord::Base
+  # contact form validations
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence:  true
+  validates :email, inclusion: { in: %w(@ .),
+  message: "%{value} isn't a valid email adress" }
   validates :message, presence: true
 end
